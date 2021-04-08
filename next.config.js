f7dfc6890dae4e744100bbcf7dbbd77bdf805dcb
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-04-08 20:23:44 +0900

    Set SASS up

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
-const withSass = require('@zeit/next-sass')
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS(withSass({
+const path = require('path')
+
+module.exports = {
   distDir: 'build_dir',
-
   assetPrefix: './',
-
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   exportPathMap: async function(
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
   ) {
     return {
       '/': { page: '/' },
+      // '/post/:id': { page: '/post/[id].tsx' },
       // '/about.md': { page: '/about' },
       // '/team.md': { page: '/team' },
       // '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } }
     }
   },
-}))
+}
