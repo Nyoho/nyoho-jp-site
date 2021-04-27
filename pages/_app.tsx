836424dc22c4d738001cf24c17634ada711a0925
北䑓如法 <algebraicallyClosedField@gmail.com>
2021-04-28 07:25:40 +0900

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import App, { Container } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 import '../styles/global.sass'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faMicrophone, faMapMarkerAlt, faTv, faCode, faPencilAlt, faBookOpen, faCameraRetro } from '@fortawesome/free-solid-svg-icons'
@@ -9,6 +12,17 @@ import { faTwitter, faFacebook, faGithub, faYoutube, faFoursquare } from '@forta
 library.add(faMicrophone, faMapMarkerAlt, faTwitter, faFacebook, faGithub, faYoutube, faFoursquare, faTv, faCode, faPencilAlt, faBookOpen, faClock, faFolderOpen, faCameraRetro)
 
 const  MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
