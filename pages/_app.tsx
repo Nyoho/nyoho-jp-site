ed9ad20f62b03532d58ed244a5d1d85122a3beb0
北䑓如法 <algebraicallyClosedField@gmail.com>
2022-08-22 21:44:07 +0900

    Remove them from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,6 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
 import '../styles/global.sass'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMicrophone, faMapMarkerAlt, faTv, faCode, faPencilAlt, faBookOpen, faCameraRetro } from '@fortawesome/free-solid-svg-icons'
-import { faClock, faFolderOpen } from '@fortawesome/free-regular-svg-icons'
-import { faTwitter, faFacebook, faGithub, faYoutube, faFoursquare } from '@fortawesome/free-brands-svg-icons'
-
-library.add(faMicrophone, faMapMarkerAlt, faTwitter, faFacebook, faGithub, faYoutube, faFoursquare, faTv, faCode, faPencilAlt, faBookOpen, faClock, faFolderOpen, faCameraRetro)
 
 const  MyApp = ({ Component, pageProps }: AppProps) => {
   const router = useRouter()
