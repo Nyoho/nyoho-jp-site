fd90846897c09c4af7d43abaf59c5d2675119475
北䑓如法 <algebraicallyClosedField@gmail.com>
2022-08-29 21:58:20 +0900

    refactor

--- components/main-content.tsx
+++ components/main-content.tsx
@@ -6,17 +6,15 @@ type Props = {
 }
 
 export default function MainContent({ content }: Props) {
-  const c = content
-
   return (
-      <div id='main-content' role='main'>
-          <SocialLinks />
-          <section id='favorites'>
-            <div className='container'>
-                <h2 id="favorites">Favorites</h2>
-                <div dangerouslySetInnerHTML={{ __html: c }} />
-            </div>
-          </section>
-      </div>
+    <div id='main-content' role='main'>
+      <SocialLinks />
+      <section id='favorites'>
+        <div className='container'>
+          <h2 id="favorites">Favorites</h2>
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+        </div>
+      </section>
+    </div>
   )
 }
